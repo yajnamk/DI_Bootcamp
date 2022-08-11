8d798735c7ef300c37a5e34d1b90f23436e84062
yajnamk <109580096+yajnamk@users.noreply.github.com>
2022-08-11 10:49:08 +0400

    commit

--- WEEK4/DAY4/Functions.py
+++ WEEK4/DAY4/Functions.py
@@ -1,12 +1,29 @@
-def hello_world(name="world", age="unknown", language="EN"):
-    if(language=="EN"):
-        print(f"hello {name}. Age is {age}")
-    elif(language=="FR"):
-        print(f"bonjour {name}. Age est {age}")
-    else:
-        print("language is not not supported")
+country = "Mauritius"
 
-hello_world("Pragassen", 24, "FR")
+def hello_world(name="world", age="unknown", language="EN"):
+    global country #will take the country inside the function #Rodrigues
+    country="Rodrigues"
+    if(language=="EN"):
+        string = f"hello {name}. Age is {age}. And I live in {country}" #Rodrigues
+    elif(language=="FR"):
+        string = f"bonjour {name}. Age est {age}"
+    else:
+        string = "language is not not supported"
+    return string
+
+def is_even(num):
+    if num%2 == 0:
+        return True
+    else:
+        return False
+
+x = hello_world("Pragassen", 24, "FR")
+print(x)
 hello_world(name="Yajna", language="FR")
 hello_world(language="Kreole")
 hello_world() #this will print hello world by default.
+
+print(country) #Mauritius
+
+print(is_even(2))
+print(is_even(15))
