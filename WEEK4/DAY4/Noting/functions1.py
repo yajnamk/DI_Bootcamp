43a496c80acf6221e79f4b1040fdc5317ce5ffdb
yajnamk <109580096+yajnamk@users.noreply.github.com>
2022-08-11 11:19:02 +0400

    commit

--- WEEK4/DAY4/Noting/functions1.py
+++ WEEK4/DAY4/Noting/functions1.py
@@ -1,14 +1,14 @@
-def my_f1():
-    print("Hello")
+def my_f1(age = 6):
+    print(f"Hello {age}")
 
-def my_f2():
+def my_f2(age):
     print("Word")
 
-def my_f3():
+def my_f3(age):
     print("This is Rick!")
 
 mytasks = [my_f1, my_f2, my_f3]
 print(mytasks)
 
 for task in mytasks:
-    task()
+    task(10)
