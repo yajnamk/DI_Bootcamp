249fe6884eac0e2501446217fa821a6095526ac8
yajnamk <109580096+yajnamk@users.noreply.github.com>
2022-08-12 10:05:51 +0400

    commit

---
+++ WEEK4/DAY4/EXERCISES/EXERCISE_XP/Question2.py
@@ -0,0 +1,7 @@
+def favorite_book(title):
+    """Display a message about someone's favorite book."""
+    print(title + " is one of my favorite books.")
+
+favorite_book('The Bridgerton')
+
+print("favorite_book)"
--- WEEK4/DAY4/Noting/Exercise.py
+++ WEEK4/DAY4/Noting/Exercise.py
@@ -1,5 +1,14 @@
 persons = ["Rick", "Morty", "Beth", "Jerry", "Snowball"]
-four_letter = filter(lambda person: len(person)<=4, persons)
+four_letter = list(filter(lambda person: len(person)<=4, persons))
 
 
-print(list(four_letter))
+print(four_letter)
+
+def hello_name(name):
+    print("hello", name)
+    return "hello, {name}"
+
+#Map 4 letters with hello name 
+
+greet = list(map(hello_name, four_letter))
+# print(greet)
