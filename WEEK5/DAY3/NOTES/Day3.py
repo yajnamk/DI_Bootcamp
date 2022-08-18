9d4bb97ef9946f4ff25bd484d7cebbfc3bd075c9
yajnamk <109580096+yajnamk@users.noreply.github.com>
2022-08-18 10:15:51 +0400

    Day 3

---
+++ WEEK5/DAY3/NOTES/Day3.py
@@ -0,0 +1,18 @@
+from typing_extensions import Self
+
+
+Class Dog:
+no_of_dogs=0
+
+def __init__(self.name):
+    self.name = name
+    print(f"creating dog {self.name}")
+    Dog.no_of_dogs += 1
+
+def running speed():
+    return self.Weight / Self.age * 10
+
+nayar_dog = Dog("Speedy")
+mikaia_dog = ("Foxxy")
+
+print(Dog.no_of_dogs)
